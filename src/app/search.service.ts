b19fe4bd4b5f34ae0f223ba87db53cdca93dbf34
Kuro Arigami <kuro@localhost.localdomain>
2019-06-10 17:55:19 -0400

    added properties username,clientid and clientsecret in search services

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/map';
+import { Injectable } from "@angular/core";
+import "rxjs/add/operator/map";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class SearchService {
+  private username: String;
+  private clientid: String = 'fff19b0b3b7efb0d955a';
+  private clientsecret: String = '89d01a204024beadd8e2184ef6adcd6416b110e7';
 
-  constructor() { }
+  constructor() {}
 }
