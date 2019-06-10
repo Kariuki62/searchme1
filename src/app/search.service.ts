fec4c9e42d7b51bec3db8fbf0a39a9b29aec0cb7
Kuro Arigami <kuro@localhost.localdomain>
2019-06-10 18:08:53 -0400

    added user function in service ts

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -15,9 +15,15 @@ export class SearchService {
 
     this.username = "Kariuki62" ;
   }
+  
+  getUserInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+   .map(res => res.json());
+  }
   getReposInfo(){
     return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
     .map(res => res.json());
   }
+
 
 }
